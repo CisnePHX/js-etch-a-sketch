962dd80e4f1673a25067fdf9219aa063514f4761
HDBarton <heatherdbarton@gmail.com>
2022-08-08 00:12:58 -0700

    add input to createGrid and cleanup code

--- functions.js
+++ functions.js
@@ -2,54 +2,51 @@
 // to make divs appear as a grid, try: float/clear, inline-block, flexbox, CSS Grid
 // space should be 960px wide
 
-// **create square size as 960 / num of squares desired for editability in future
-let setSquare = 16;
-let totalSquares = 16 * 16;
-let squareSize = 960/setSquare;
-squareSize = squareSize + "px";
 
-// **create a loop to create setSquare number of div elements with setSquare number of divs inside it (one for each square)
 
-const grid = document.getElementById('grid');
-grid.style.display = "flex";
-grid.style.flexDirection = "row";
+function mouseOver(myDiv) {
+    myDiv.style.backgroundColor = "white";
+}
 
-// **create "hover" effect by changing background color to edit CSS in javascript via their class id (leaving a pixelated trail)
-// **give each div a onmouseover and onmouseout of changing background color to white
+function createGrid(numSquare, grid){
+    let squareSize = 960/(numSquare);
+    squareSize = squareSize + "px";
 
-//creates the rows of divs
-for (h = 0; h < setSquare; h++){
-    let newDivRow = document.createElement('div')
-    grid.append(newDivRow);
-    newDivRow.className += "divRow";
-    //creates the columns of divs and each square (background and event listener included)
-    for (i = 0; i < setSquare; i++){
-        let newDiv = document.createElement('div');
+    //creates the rows of divs
+    for (h = 0; h < numSquare; h++){
+        let newDivRow = document.createElement('div')
+        grid.append(newDivRow);
+        newDivRow.className += "divRow";
+        console.log("row ", h);
+        //creates the columns of divs and each square (background and event listener included)
+        for (i = 0; i < numSquare; i++){
+            let newDiv = document.createElement('div');
+            
+            console.log("square ", i);
 
-        newDivRow.append(newDiv);
-        newDiv.id += "square";
-        newDiv.style.height = squareSize;
-        newDiv.style.width = squareSize;
-        newDiv.style.backgroundColor= "black";
-        newDiv.onmouseover = function() {mouseOver()};
-        newDiv.onmouseout = function () {mouseOut()};
+            newDiv.id += "square";
+            newDiv.style.height = squareSize;
+            newDiv.style.width = squareSize;
+            newDiv.style.backgroundColor= "black";
+            newDiv.onmouseover = function() {mouseOver(newDiv)};
+            newDivRow.append(newDiv);
 
-        function mouseOver() {
-            newDiv.style.backgroundColor = "white";
-        }
-        function mouseOut() {
-            newDiv.style.backgroundColor = "white";
+            
         }
     }
 }
 
 // Need a button that creates a popup asking for the desired number of squares
-// removes old grid and new grid appears in the space of the same size by refreshing page with new setSquare value
+// removes old grid and new grid appears in the space of the same size by refreshing page with new defaultSquare value
 // limit is a max of 100px wide//
 // look into how to make a js script run after a button is pushed
 // look into prompts
 
-document.querySelector("#btn").addEventListener('click', onPrompt);
+function deleteGrid(){
+    while (grid.hasChildNodes()){
+        grid.removeChild(grid.firstChild);
+    }
+}
 
 function onPrompt() {
     let sizeRequest = prompt("How many squares would you like there to be per side? (less than 100 only)");
@@ -58,15 +55,32 @@ function onPrompt() {
         if (sizeRequest > 100) {
             alert("Must be less than 100!");
         }else {
-            setSquare = sizeRequest;
+            defaultSquare = sizeRequest;
         }
     }
 }
 
+// **create square size as 960 / num of squares desired for editability in future
+let defaultSquare = 16;
+let currentSquare = 16;
+let totalSquares = 16 * 16;
 
+// **create a loop to create defaultSquare number of div elements with defaultSquare number of divs inside it (one for each square)
+
+const grid = document.getElementById('grid');
+grid.style.display = "flex";
+grid.style.flexDirection = "row";
+
+// **create "hover" effect by changing background color to edit CSS in javascript via their class id (leaving a pixelated trail)
+// **give each div a onmouseover and onmouseout of changing background color to white
+//defaultSquare should be numSquare
 
 
 
 // Extra Credit: Instead of just changing the color of a square from black to white (for example), have each pass through 
 // with the mouse change it to a completely random RGB value. Then try having each pass just add another 10% of black to it 
 // so that only after 10 passes is the square completely black.
+
+
+createGrid(defaultSquare, grid);
+document.querySelector("#btn").addEventListener('click', onPrompt);
